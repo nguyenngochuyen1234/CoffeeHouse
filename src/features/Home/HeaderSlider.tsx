61e6d70500fa6f185f291a04b61f38fe246b5927
stark8193 <maibui11111@gmail.com>
2024-01-04 21:53:14 +0700

    new

--- src/features/Home/HeaderSlider.tsx
+++ src/features/Home/HeaderSlider.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Carousel, Image } from "antd"
+import slider1 from '../../assets/web_moi_-_desktop_b5a00ce640cf431b90b75feee7ab480d.webp'
+import slider2 from '../../assets/desktop_9385860226df4a81afcedf520f9a563f.webp'
+import slider3 from '../../assets/desktop_9c03e90bd535442e9ac18d23fc551cb6.webp'
 
 const SliderHeader = () => {
+  // const imgArray = [{slider1},{slider2},{slider3}]
   return (
-    <div className='h-[410px] flex justify-center items-center bg-[#fdff8b]'>Slider</div>
+    <Carousel autoplay className="absolute top-0 left-0 w-full h-full">
+      <div>
+      <Image src={slider1} alt="slider" preview={false}/>
+    </div>
+    <div>
+      <Image src={slider2} alt="slider" preview={false}/>
+    </div>
+    <div>
+      <Image src={slider3} alt="slider" preview={false}/>
+    </div>
+  </Carousel>
   )
 }
 
