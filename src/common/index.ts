c464c83b35b57eb971d0361fe787a5f45d224e7d
stark8193 <maibui11111@gmail.com>
2024-02-16 22:55:29 +0700

    a

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,1 +1,2 @@
-export const STATIC_HOST = "https://coffeehouse-backend.onrender.com/"
+export const STATIC_HOST = "http://localhost:8800/"
+// export const STATIC_HOST = "https://coffeehouse-backend.onrender.com/"
